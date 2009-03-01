64b15f2fedd42d9164a3e26b95d9596daca89998
johannes.dellert <johannes.dellert@b7f6f1da-d9bc-11dd-93da-dbb4cac96d7c>
2009-03-01 12:53:38 +0000

    - inital commit for lc-parser demo architecture - contains many useful snippets of jasper code

---
+++ trunk/src/lc-parser/src/TreeViewNode.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+public class TreeViewNode 
+{
+	TreeModelNode modelNode;
+    int id;
+    int parent;
+    ArrayList<Integer> children;
+    String tag;
+    int x;
+    int y;
+    int subTreeWidth;
+    String edgeTag;
+    String edgeDir;
+	String edgeType;
+    
+    public TreeViewNode(TreeModelNode modelNode, int parent, ArrayList<Integer> children, int x, int y)
+    {
+        this.modelNode = modelNode;
+        this.id = modelNode.id;
+        this.parent = parent;
+        this.children = children;
+        this.tag = modelNode.content;
+        this.x = x;
+        this.y = y;
+        this.subTreeWidth = 1;
+        this.edgeTag = "";
+        this.edgeDir = "";
+        this.edgeType = "";
+        //star at beginning of tag symbolizes dotted edge to parent
+        if (tag.startsWith("*"))
+        {
+            tag = tag.substring(1);
+            edgeType = "dotted";
+        }
+    }
+}
