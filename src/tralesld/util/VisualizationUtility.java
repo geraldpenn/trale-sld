fc286793f803763516fcea7367d05d4f4acb148e
kilian.evang <kilian.evang@b7f6f1da-d9bc-11dd-93da-dbb4cac96d7c>
2009-03-22 23:21:18 +0000

    VisualizationUtility now accepts Grisu strings

--- src/tralesld/util/VisualizationUtility.java
+++ src/tralesld/util/VisualizationUtility.java
@@ -1,25 +1,46 @@
 package tralesld.util;
 
 import gralej.blocks.BlockPanel;
-import gralej.om.IVisitable;
+import gralej.controller.StreamInfo;
+import gralej.parsers.GraleParserFactory;
+import gralej.parsers.IGraleParser;
+import gralej.parsers.ParseException;
+import gralej.parsers.UnsupportedProtocolException;
+
+import java.io.ByteArrayInputStream;
+
 import javax.swing.JPanel;
 
 /**
- *
+ * 
  * @author ke
  */
 public class VisualizationUtility {
 
+    private IGraleParser parser;
+
+    public VisualizationUtility() {
+	try {
+	    parser = GraleParserFactory.createParser(StreamInfo.GRISU);
+	} catch (UnsupportedProtocolException e) {
+	    throw new RuntimeException("could not create Grisu format parser",
+		    e);
+	}
+    }
+
     /**
      * 
-     * @param fs A typed feature structure in Gralej's internal representation.
+     * @param grisuMessage
+     *            A typed feature structure in Grisu format.
      * @return An object representing the visualization of the feature
-     *         structure, providing various methods to control rendering,
-     *         and a method called <code>getCanvas()</code> to obtain the
-     *         actual {@link JPanel}.
+     *         structure, providing various methods to control rendering, and a
+     *         method called <code>getCanvas()</code> to obtain the actual
+     *         {@link JPanel}.
      */
-    public static BlockPanel visualizeTypedFS(IVisitable fs) {
-        return (new BlockPanel(fs));
+    public BlockPanel visualize(String grisuMessage) throws ParseException {
+	return parser.parseAll(
+		new ByteArrayInputStream(grisuMessage.getBytes()),
+		StreamInfo.GRISU).get(0).createView();
     }
 
 }
