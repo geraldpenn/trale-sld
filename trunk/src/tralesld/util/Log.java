aac99b23f16cf68329c510aac6f5f19f4cc8e56e
kilian.evang <kilian.evang@b7f6f1da-d9bc-11dd-93da-dbb4cac96d7c>
2009-03-27 01:18:44 +0000

    testing resurrected Subclipse

--- trunk/src/tralesld/util/Log.java
+++ trunk/src/tralesld/util/Log.java
@@ -4,30 +4,30 @@ import org.apache.log4j.Logger;
 
 public class Log {
 
-    private static org.apache.log4j.Logger logger = Logger.getLogger(Log.class);
-    
-    public static void trace(Object message) {
-	logger.trace(message);
-    }
-    
-    public static void debug(Object message) {
-	logger.debug(message);
-    }
-    
-    public static void info(Object message) {
-	logger.info(message);
-    }
-    
-    public static void warn(Object message) {
-	logger.warn(message);
-    }
-    
-    public static void error(Object message) {
-	logger.error(message);
-    }
-    
-    public static void fatal(Object message) {
-	logger.fatal(message);
-    }
+	private static org.apache.log4j.Logger logger = Logger.getLogger(Log.class);
+
+	public static void trace(Object message) {
+		logger.trace(message);
+	}
+
+	public static void debug(Object message) {
+		logger.debug(message);
+	}
+
+	public static void info(Object message) {
+		logger.info(message);
+	}
+
+	public static void warn(Object message) {
+		logger.warn(message);
+	}
+
+	public static void error(Object message) {
+		logger.error(message);
+	}
+
+	public static void fatal(Object message) {
+		logger.fatal(message);
+	}
 
 }
