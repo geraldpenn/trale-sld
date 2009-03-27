bb1a3ffde243d51c4e07b8b16cc4d1ef58d988c4
kilian.evang <kilian.evang@b7f6f1da-d9bc-11dd-93da-dbb4cac96d7c>
2009-03-27 19:57:14 +0000

    the Prolog side of today's changes (cf. r58-r66), mainly calls at fail, finished, exit ports, more informative arguments
    KNOWN BUG: wrong "Right" parameter given for rule applications, will be right on that...

--- trunk/trale/debugger/tralesld_hooks.pl
+++ trunk/trale/debugger/tralesld_hooks.pl
@@ -32,8 +32,12 @@ call_foreign_meta(JVM, Goal) :-
 % registry of all the GUI methods that have to be called from within Prolog 
 foreign(method('tralesld/TraleSld','initializeParseTrace',[instance]),java,init_parse_trace(+object('tralesld.TraleSld'),+chars)).
 foreign(method('tralesld/TraleSld','registerChartEdge',[instance]),java,register_chart_edge(+object('tralesld.TraleSld'),+integer,+integer,+integer,+chars)).
-foreign(method('tralesld/TraleSld','registerStepLocation',[instance]),java,register_step_location(+object('tralesld.TraleSld'),+chars)).
 foreign(method('tralesld/TraleSld','registerStepInformation',[instance]),java,register_step_information(+object('tralesld.TraleSld'),+integer,+chars)).
+foreign(method('tralesld/TraleSld','registerStepLocation',[instance]),java,register_step_location(+object('tralesld.TraleSld'),+chars)).
+foreign(method('tralesld/TraleSld','registerRuleApplication',[instance]),java,register_rule_application(+object('tralesld.TraleSld'),+integer,+integer,+integer,+chars)).
+foreign(method('tralesld/TraleSld','registerStepFailure',[instance]),java,register_step_failure(+object('tralesld.TraleSld'),+chars)).
+foreign(method('tralesld/TraleSld','registerStepFinished',[instance]),java,register_step_finished(+object('tralesld.TraleSld'),+chars)).
+foreign(method('tralesld/TraleSld','registerStepExit',[instance]),java,register_step_exit(+object('tralesld.TraleSld'),+chars)).
 foreign(method('tralesld/TraleSld','getPressedButton',[instance]),java,get_pressed_button(+object('tralesld.TraleSld'),[-char])).
 
 % Fire up one JVM and store it for future use
@@ -41,7 +45,7 @@ load_jvm_if_necessary :-
     jvm_store(_).
     
 load_jvm_if_necessary :-
-    jasper_initialize([classpath('/home/ke/java/trale-sld/build/classes')],JVM),
+    jasper_initialize([classpath('/home/ke/workspace/trale-sld/bin')],JVM),
     assert(jvm_store(JVM)).
 
 % Load one instance of the graphical SLD
@@ -63,21 +67,43 @@ tralesld_parse_begin(Words) :-
 
 % Called before a new step first appears on the stack to transmit information
 % about this step to the GUI. The purpose is to keep the stack lean, with just
-% step IDs and no further information about the steps on it. Currently, the
-% CommandName is the only information associated with a step, this will change
-% significantly.
-tralesld_register_step_information(ID, CommandName) :-
+% step IDs and no further information about the steps on it.
+tralesld_step(ID, step(rule(RuleName),
+                       _Line,
+                       d_add_dtrs(_,_,Left,Right,_,_,Seven,Eight,Nine,_,Eleven,Twelve))) :-
+%write(-), write(Seven), nl,
+%write(-), write(Eight), nl,
+%write(-), write(Nine), nl,
+%write(-), write(Eleven), nl,
+%write(-), write(Twelve), nl,
+    !,
     jvm_store(JVM),
-    write_to_chars(CommandName, CommandNameChars),
     gui_store(JavaSLD),
-    call_foreign_meta(JVM, register_step_information(JavaSLD, ID, CommandNameChars)).
+    write_to_chars(RuleName,RuleNameChars),
+    call_foreign_meta(JVM,register_rule_application(JavaSLD,ID,Left,Right,RuleNameChars)).
+
+tralesld_step(ID, step(Command,_Line,_Goal)) :-
+    jvm_store(JVM),
+    gui_store(JavaSLD),
+    write_to_chars(Command,CommandChars), % TODO
+    atom_chars(CommandAtom,CommandChars), 
+    shorten(CommandAtom,ShortenedAtom),
+    atom_chars(ShortenedAtom,ShortenedChars),
+    call_foreign_meta(JVM,register_step_information(JavaSLD,ID,ShortenedChars)).
+
+shorten(Atom,Shortened) :-
+    sub_atom(Atom,0,25,_,Prefix),
+    atom_concat(Prefix,'...',Shortened),
+    !.
+
+shorten(Atom,Atom).
 
 % The following predicates are called with the current stack as an argument,
 % containing integer step IDs.
 
 % Called when a step is first called. Stack already contains the ID of this
 % step.
-tralesld_stack_at_call_port(Stack) :-
+tralesld_call(Stack) :-
   jvm_store(JVM),
   gui_store(JavaSLD),
   write_to_chars(Stack, StackChars),
@@ -86,30 +112,45 @@ tralesld_stack_at_call_port(Stack) :-
 % Called when a step fails. Stack does not contain the failed step any more.
 tralesld_stack_at_fail_port(Stack).
 
-% Called when a failure-driven step completes (i.e. fails). Stack does not
-% contain the step any more.
-tralesld_stack_at_fail_port(Stack).
+% Called when a failure-driven step completes (i.e. fails). Stack still
+% contains the step.
+tralesld_fail(Stack) :-
+    jvm_store(JVM),
+    gui_store(JavaSLD),
+    write_to_chars(Stack, StackChars),
+    call_foreign_meta(JVM, register_step_failure(JavaSLD, StackChars)).
 
-tralesld_stack_at_finished_port(Stack).
+% Called when a failure-driven step completes.
+tralesld_finished(Stack) :-
+    jvm_store(JVM),
+    gui_store(JavaSLD),
+    write_to_chars(Stack, StackChars),
+    call_foreign_meta(JVM, register_step_finished(JavaSLD, StackChars)).
 
-% Called when a step completes successfully.
-tralesld_stack_at_exit_port(Stack).
+% Called when a step completes successfully. Stack  still contains the step.
+tralesld_exit(Stack) :-
+    jvm_store(JVM),
+    gui_store(JavaSLD),
+    write_to_chars(Stack, StackChars),
+    call_foreign_meta(JVM, register_step_exit(JavaSLD, StackChars)).
 
 % Called when a previously successful step is redone.
-tralesld_stack_at_redo_port(Stack).
+tralesld_redo(Stack).
 
 % Called when an edge is added to the chart (happens as a side effect during
 % application of a rule.
 tralesld_edge_added(Number,Left,Right,RuleName) :-
-	jvm_store(JVM),
-	gui_store(JavaSLD),
-        write_to_chars(RuleName, RuleNameChars),
-	call_foreign_meta(JVM,register_chart_edge(JavaSLD,Number,Left,Right,RuleNameChars)).
+    jvm_store(JVM),
+    gui_store(JavaSLD),
+    write_to_chars(RuleName, RuleNameChars),
+    call_foreign_meta(JVM,register_chart_edge(JavaSLD,Number,Left,Right,RuleNameChars)).
 
 % Called by the debugger to retrieve instructions from the GUI
 get_reply_hook(Reply) :-
     parsing(_),
-    await_gui_guidance(Reply).
+    await_gui_guidance(Reply),
+    write(Reply),
+    nl.
 
 await_gui_guidance(Pressed) :-
     wait_until_button_pressed(Pressed),
@@ -119,13 +160,15 @@ wait_until_button_pressed(Pressed) :-
     repeat,
         sleep(0.1),
         pressed_button(Pressed),
-        (
-          Pressed ==  97; % (a)bort
-          Pressed ==  99; % (c)reep
-          Pressed == 102  % (f)ail
-         %Pressed == 108; % (l)eap TODO support
-         %Pressed == 115;  % (s)kip TODO support
-        ).
+%write(Pressed), nl,
+        \+ Pressed == 110.
+%        (
+%          Pressed ==  97; % (a)bort
+%          Pressed ==  99; % (c)reep
+%          Pressed == 102  % (f)ail
+%         %Pressed == 108; % (l)eap TODO support
+%         %Pressed == 115;  % (s)kip TODO support
+%        ).
 
 pressed_button(Button) :-
     jvm_store(JVM),
@@ -146,50 +189,45 @@ pressed_button(Button) :-
 announce_parse_begin_hook(Words) :-
   retractall(sid_stack(_)),
   retractall(sid_next_step(_)),
-  asserta(sid_stack([])),
+  asserta(sid_stack([0])),
   tralesld_parse_begin(Words).
 
 announce_step_hook(StepID,Command,Line,Goal) :-
-  write('announce_step_hook called'), nl,
   sid_set_next_step(StepID),
-  Command =.. [CommandName|_],
-  write('trying to register step information... '),
-  tralesld_register_step_information(StepID, CommandName), % TODO this will eventually contain much more information than just the command name
+write('Command: '), write(Command), nl,
+write('Goal: '), write(Goal), nl,
+  tralesld_step(StepID, step(Command,Line,Goal)), % TODO this will eventually contain much more information than just the command name
   write('success'), nl.
 
 announce_call_hook :-
-  write('announce_call_hook called'), nl,
   sid_next_step(StepID),
   sid_push(StepID),
   sid_stack(Stack),
-write('Stack: '), write(Stack), nl,
-  write('trying to register step location... '),
-  tralesld_stack_at_call_port(Stack),
-  write('success'), nl.
+  tralesld_call(Stack).
 
 announce_fail_hook :-
+  sid_stack(OldStack),
   sid_pop(StepID),
   sid_set_next_step(StepID), % may be retried
-  sid_stack(Stack),
-  tralesld_stack_at_fail_port(Stack).
+  tralesld_fail(OldStack).
 
 announce_finished_hook :-
+  sid_stack(OldStack),
   sid_pop(StepID),
   sid_set_next_step(StepID), % may be retried
-  sid_stack(Stack),
-  tralesld_stack_at_finished_port(Stack).
+  tralesld_finished(OldStack).
 
 announce_exit_hook :-
+  sid_stack(OldStack),
   sid_pop(StepID),
   sid_set_next_step(StepID), % may be retried
-  sid_stack(Stack),
-  tralesld_stack_at_exit_port(Stack).
+  tralesld_exit(OldStack).
 
 announce_redo_hook(StepID) :-
   sid_push(StepID),
   sid_set_next_step(StepID), % may be retried
   sid_stack(Stack),
-  tralesld_stack_at_redo_port(Stack).
+  tralesld_redo(Stack).
   
 announce_edge_added_hook(Number,Left,Right,RuleName) :-
   tralesld_edge_added(Number,Left,Right,RuleName).
