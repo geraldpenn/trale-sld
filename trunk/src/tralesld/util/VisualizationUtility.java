f95eba37a670cca0544b0dd40e775af358857431
kilian.evang <kilian.evang@b7f6f1da-d9bc-11dd-93da-dbb4cac96d7c>
2009-03-30 12:45:54 +0000

    - reverted accidental reversal of r41

--- trunk/src/tralesld/util/VisualizationUtility.java
+++ trunk/src/tralesld/util/VisualizationUtility.java
@@ -1,17 +1,46 @@
 package tralesld.util;
 
 import gralej.blocks.BlockPanel;
-import gralej.om.IVisitable;
+import gralej.controller.StreamInfo;
+import gralej.parsers.GraleParserFactory;
+import gralej.parsers.IGraleParser;
+import gralej.parsers.ParseException;
+import gralej.parsers.UnsupportedProtocolException;
+
+import java.io.ByteArrayInputStream;
+
 import javax.swing.JPanel;
 
 /**
- *
+ * 
  * @author ke
  */
 public class VisualizationUtility {
 
-    public static JPanel visualizeTypedAVM(IVisitable avm) {
-        return (new BlockPanel(avm)).getCanvas();
-    }
+	private IGraleParser parser;
+
+	public VisualizationUtility() {
+		try {
+			parser = GraleParserFactory.createParser(StreamInfo.GRISU);
+		} catch (UnsupportedProtocolException e) {
+			throw new RuntimeException("could not create Grisu format parser",
+					e);
+		}
+	}
+
+	/**
+	 * 
+	 * @param grisuMessage
+	 *            A typed feature structure in Grisu format.
+	 * @return An object representing the visualization of the feature
+	 *         structure, providing various methods to control rendering, and a
+	 *         method called <code>getCanvas()</code> to obtain the actual
+	 *         {@link JPanel}.
+	 */
+	public BlockPanel visualize(String grisuMessage) throws ParseException {
+		return parser.parseAll(
+				new ByteArrayInputStream(grisuMessage.getBytes()),
+				StreamInfo.GRISU).get(0).createView();
+	}
 
 }
