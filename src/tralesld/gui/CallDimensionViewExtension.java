8f886f761031b8fb1d83a77d47165b56e6d38c88
johannes.dellert <johannes.dellert@b7f6f1da-d9bc-11dd-93da-dbb4cac96d7c>
2009-04-04 17:55:59 +0000

    - improved display of call stack dimension in decision tree

--- src/tralesld/gui/CallDimensionViewExtension.java
+++ src/tralesld/gui/CallDimensionViewExtension.java
@@ -39,11 +39,34 @@ public class CallDimensionViewExtension extends TreeViewExtension
 					{
 						canvas.setColor(Color.LIGHT_GRAY);
 					}
-					int leftX = parent.x;
+					/*int leftX = parent.x;
 					int width = Math.abs(n.x - leftX) + 50;
 					if (n.x < leftX) leftX = n.x;
 					int arcAngle = (int) Math.round((Math.atan((parent.x - n.x) / (n.y - parent.y) * 0.5) * 180)/Math.PI);
-					canvas.drawArc(parent.x - width, parent.y, width * 2, n.y - parent.y, 90, 180 - arcAngle);
+					canvas.drawArc(parent.x - width, parent.y, width * 2, n.y - parent.y, 90, 180 - arcAngle);*/
+					int c = 50;
+					int a = Math.abs(n.x - parent.x) + c;
+					int x = Math.abs(n.x - parent.x);
+					int y = n.y - parent.y;
+					int b = y / 2;
+					if (x != 0)
+					{
+						b = (int) (((a - Math.sqrt(a*a-x*x)) * y * a) / (x * x)); 
+					}
+					int arcAngle = 180;
+					if (x != 0)
+					{	
+						arcAngle = (int) Math.round((Math.atan((double)(y - b)/((x/(x+50.0))*b)) * 180)/Math.PI);
+						if (parent.x - n.x < 0)
+						{
+							arcAngle = 270 - arcAngle; 
+						}
+						else
+						{
+							arcAngle += 90;
+						}
+					}
+					canvas.drawArc(parent.x - a, parent.y, 2 * a, 2 * b, 90, arcAngle);
 				}
 			}
 		}
