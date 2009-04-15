ef187cec5f4aa1de0b4e82b33d3e45585f96e913
kilian.evang <kilian.evang@b7f6f1da-d9bc-11dd-93da-dbb4cac96d7c>
2009-04-15 18:00:56 +0000

    - toString method for DataStore

--- src/tralesld/storage/DataStore.java
+++ src/tralesld/storage/DataStore.java
@@ -1,39 +1,51 @@
 package tralesld.storage;
+
 import java.util.*;
 
 public class DataStore<T>
 {
-    HashMap<Integer,T> parsingData;
-    
-    public DataStore()
-    {
-        parsingData = new HashMap<Integer,T>();
-    }
-    
-    public T getData(int id)
-    {
-        if (id == -1)
-        {
-            return null;
-        }
-        else
-        {
-            return parsingData.get(id);
-        }
-    }
-    
-    public void put(int index, T item)
-    {
-    	parsingData.put(index, item);
-    }
-    
-    public int size()
-    {
-    	return parsingData.size();
-    }
-    
-    public Set<Integer> getKeySet()
-    {
-    	return parsingData.keySet();
-    }
+	HashMap<Integer, T> parsingData;
+
+	public DataStore()
+	{
+		parsingData = new HashMap<Integer, T>();
+	}
+
+	public T getData(int id)
+	{
+		if (id == -1)
+		{
+			return null;
+		}
+		else
+		{
+			return parsingData.get(id);
+		}
+	}
+
+	public void put(int index, T item)
+	{
+		parsingData.put(index, item);
+	}
+
+	public int size()
+	{
+		return parsingData.size();
+	}
+
+	public Set<Integer> getKeySet()
+	{
+		return parsingData.keySet();
+	}
+
+	public String toString()
+	{
+		StringBuilder builder = new StringBuilder();
+		
+		for (int i : parsingData.keySet()) {
+			builder.append(i + " => " + parsingData.get(i) + ", ");
+		}
+
+		return builder.toString();
+	}
 }
