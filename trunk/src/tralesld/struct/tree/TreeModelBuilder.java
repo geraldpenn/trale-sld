897d8d291e533ac71b9b11ac273a2b539b035604
kilian.evang <kilian.evang@b7f6f1da-d9bc-11dd-93da-dbb4cac96d7c>
2010-03-01 14:16:41 +0000

    Removed unused class.

--- trunk/src/tralesld/struct/tree/TreeModelBuilder.java
+++
@@ -1,7 +0,0 @@
-package tralesld.struct.tree;
-
-public abstract class TreeModelBuilder<T> 
-{        
-	//tree model constructor to be implemented
-	public abstract TreeModel createTreeModel(T t);
-}
